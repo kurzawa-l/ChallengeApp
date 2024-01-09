95c064f3e35386ad08758a24dc05b5cb2866668e
walkerek <kurzawa.l@wp.pl>
2024-01-09 12:37:00 +0100

    Dzień 4

--- ChallengeApp/ChallengeApp/Program.cs
+++ ChallengeApp/ChallengeApp/Program.cs
@@ -1,2 +1,18 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, Łukasz!");
+﻿string name = "Joanna";
+var myAge = 27;
+char sex = 'K';
+
+
+
+if (name == "Ewa" && myAge == 30 && sex == 'K')
+{
+    Console.WriteLine("Ewa lat 30");
+}
+else if (sex == 'K' && myAge < 30)
+{
+    Console.WriteLine("Kobieta poniżej 30 lat.");
+}
+else if (sex == 'M' && myAge < 18)
+{
+    Console.WriteLine("Niepełnoletni Mężczyzną.");
+}
